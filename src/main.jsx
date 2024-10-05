5db28f0674d5d4c14739db2adc9461a34cd9275f
zhassan2312 <zhassan.ce44ceme@student.nust.edu.pk>
2024-10-05 16:27:45 +0500

    temporarily removed sentry

--- src/main.jsx
+++ src/main.jsx
@@ -5,24 +5,24 @@ import './index.css'
 import React,{useEffect} from 'react'
 import * as Sentry from "@sentry/react"
 
-Sentry.init({
-  dsn: "https://fcac04853352ef16c40108785981f908@o4508009590816768.ingest.us.sentry.io/4508009592324096",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration(
-      {
-        useEffect:useEffect,
-      }
-    ),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, 
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, 
-  replaysOnErrorSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: "https://fcac04853352ef16c40108785981f908@o4508009590816768.ingest.us.sentry.io/4508009592324096",
+//   integrations: [
+//     Sentry.browserTracingIntegration(),
+//     Sentry.reactRouterV6BrowserTracingIntegration(
+//       {
+//         useEffect:useEffect,
+//       }
+//     ),
+//     Sentry.replayIntegration(),
+//   ],
+//   // Tracing
+//   tracesSampleRate: 1.0, 
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, 
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
