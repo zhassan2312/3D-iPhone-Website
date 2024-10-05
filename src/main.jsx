3059e1d31c2146ab9fea900f3eabe409ac70b2b7
zhassan2312 <zhassan.ce44ceme@student.nust.edu.pk>
2024-10-05 16:21:16 +0500

    fix issues

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import React from 'react'
-import * as Sentry from "@sentry/react";
+import React,{useEffect} from 'react'
+import * as Sentry from "@sentry/react"
 
 Sentry.init({
   dsn: "https://fcac04853352ef16c40108785981f908@o4508009590816768.ingest.us.sentry.io/4508009592324096",
@@ -11,7 +11,7 @@ Sentry.init({
     Sentry.browserTracingIntegration(),
     Sentry.reactRouterV6BrowserTracingIntegration(
       {
-        useEffect:React.useEffect,
+        useEffect:useEffect,
       }
     ),
     Sentry.replayIntegration(),
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/3D-iPhone-Website/',
   plugins: [
     react(),
     sentryVitePlugin({
