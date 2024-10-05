18e61d574399640b96690e15fc1e2a87268bd904
zhassan2312 <zhassan.ce44ceme@student.nust.edu.pk>
2024-10-05 16:49:47 +0500

    error fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import React,{useEffect} from 'react'
-import * as Sentry from "@sentry/react"
+import React from 'react'
+// import * as Sentry from "@sentry/react"
 
 // Sentry.init({
 //   dsn: "https://fcac04853352ef16c40108785981f908@o4508009590816768.ingest.us.sentry.io/4508009592324096",
@@ -17,10 +17,10 @@ import * as Sentry from "@sentry/react"
 //     Sentry.replayIntegration(),
 //   ],
 //   // Tracing
-//   tracesSampleRate: 1.0, 
+//   tracesSampleRate: 1.0,
 //   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
 //   // Session Replay
-//   replaysSessionSampleRate: 0.1, 
+//   replaysSessionSampleRate: 0.1,
 //   replaysOnErrorSampleRate: 1.0,
 // });
 
